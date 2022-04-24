5bbe090c9abb757e51960971119df3d5870eca08
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-04-24 14:27:23 +0200

    made the initial server setup

---
+++ simple_echo_server/app.js
@@ -0,0 +1,8 @@
+const HTTP = require("http");
+const PORT = 3000;
+
+const SERVER = HTTP.createServer((req, res) => {});
+
+SERVER.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
