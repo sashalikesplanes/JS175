61212d79571e132eeab05ed4154be2e222694dfa
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-04-26 19:30:20 +0200

    added navigation bar between language pages

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -7,7 +7,23 @@ app.set("view engine", "pug");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
+  res.redirect("/english");
+});
+
+app.get("/english", (req, res) => {
   res.render("hello-world-english");
+});
+
+app.get("/french", (req, res) => {
+  res.render("hello-world-french");
+});
+
+app.get("/serbian", (req, res) => {
+  res.render("hello-world-serbian");
+});
+
+app.get("/russian", (req, res) => {
+  res.render("hello-world-russian");
 });
 
 app.listen(3000, "localhost", () => {
