39a5126309d30092da339b459bef662180d00ec9
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-04-28 19:03:05 +0200

    add logging

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -1,41 +1,31 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
-
-const writeLog = (req, res) => {
-  let timeStamp = String(new Date()).substring(4, 24);
-  console.log(
-    `${timeStamp} ${req.method} ${req.originalUrl} ${res.statusCode}`
-  );
-};
+app.use(morgan("common"));
 
 app.get("/", (req, res) => {
   res.redirect("/english");
-  writeLog(req, res);
 });
 
 app.get("/english", (req, res) => {
   res.render("hello-world-english");
-  writeLog(req, res);
 });
 
 app.get("/french", (req, res) => {
   res.render("hello-world-french");
-  writeLog(req, res);
 });
 
 app.get("/serbian", (req, res) => {
   res.render("hello-world-serbian");
-  writeLog(req, res);
 });
 
 app.get("/russian", (req, res) => {
   res.render("hello-world-russian");
-  writeLog(req, res);
 });
 
 app.listen(3000, "localhost", () => {
