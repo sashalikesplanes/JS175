50b56e545d61e24103a9f0f5b1da5ea583aa833b
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-04-30 15:54:19 +0200

    completed hello-world app

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -28,35 +28,49 @@ const COUNTRY_DATA = [
     title: "Иди на русский сайт",
   },
 ];
+const LANGUAGE_CODES = {
+  english: "en-US",
+  french: "fr-FR",
+  serbian: "sr-Cryl-rs",
+  russian: "ru-RU",
+};
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
 app.use(morgan("common"));
+app.use((err, req, res, _next) => {
+  console.log(err);
+  res.status(404).send(err.message);
+});
 
 app.locals.currentPathClass = (path, currentPath) => {
   return path === currentPath ? "current" : "";
-};
-
-const helloWorld = (view, language) => {
-  return (req, res) => {
-    res.render(view, {
-      countries: COUNTRY_DATA,
-      currentPath: req.path,
-      language: language,
-    });
-  };
 };
 
 app.get("/", (req, res) => {
   res.redirect("/english");
 });
 
-app.get("/english", helloWorld("hello-world-english", "en-GB"));
-app.get("/french", helloWorld("hello-world-french", "fr-FR"));
-app.get("/serbian", helloWorld("hello-world-serbian", "sr-Cyrl-rs"));
-app.get("/russian", helloWorld("hello-world-russian", "ru-RU"));
+app.get("/:language", (req, res, next) => {
+  const language = req.params.language;
+  const languageCode = LANGUAGE_CODES[language];
+  if (!languageCode) {
+    next(new Error(`Language not supported: ${language}`));
+  } else {
+    res.render(`hello-world-${language}`, {
+      countries: COUNTRY_DATA,
+      currentPath: req.path,
+      language: languageCode,
+    });
+  }
+});
+
+// app.get("/english", helloWorld("hello-world-english", "en-GB"));
+// app.get("/french", helloWorld("hello-world-french", "fr-FR"));
+// app.get("/serbian", helloWorld("hello-world-serbian", "sr-Cyrl-rs"));
+// app.get("/russian", helloWorld("hello-world-russian", "ru-RU"));
 
 app.listen(3000, "localhost", () => {
   console.log("Listening on port 3000");
